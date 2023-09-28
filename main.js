7d127c5f00b1c7ce03b36339540d55b5188f58ec
José Enrique de la Torre Leyva <kikedlt27@gmail.com>
2023-09-27 21:28:29 -0600

    Added more sections
    -

--- main.js
+++ main.js
@@ -40,4 +40,47 @@ while(promedio >= 9){
   }
   break;
 }
+//Objetos
+const listaCalifas ={
+  calificacionHist : 9,
+  calificacionCien: 8,
+  calificacionEco:10
+}
+
+console.log(nombreEstudiante + " obtuvo un " + calificacionMate + " en la asignatura de Matematicas")
+console.log(nombreEstudiante + " obtuvo un " + calificacionEsp + " en la asigmatura de Español")
+console.log(nombreEstudiante + " obtuvo un " + calificacionIng + " en la asignatura de Ingles")
+console.log(nombreEstudiante + " obtuvo un " + calificacionGeo + " en la asignatura de Geografia")
+console.log(nombreEstudiante + " obtuvo un " + listaCalifas.calificacionHist + " en la asignatura de Historia")
+console.log(nombreEstudiante + " obtuvo un " + listaCalifas.calificacionCien + " en la asignatura de Ciencias")
+console.log(nombreEstudiante + " obtuvo un " + listaCalifas.calificacionEco + " en la Asigntarua de Economia" )
+
+//Arreglos
+const califasDeLeo = [
+  {
+    nombre: "Leonel",
+    calificacionMate1: 8,
+    calificacionEsp1: 9,
+    calificacionIng1: 10,
+    calificacionGeo1: 8,
+    calificacionHist1: 9,
+    calificacionCien1: 10,
+    calificacionEco1: 10
+  }
+]
+
+const calisLeo = califasDeLeo;
+
+console.table(calisLeo)
+
+const sumaCalisLeo = calisLeo.calificacionMate1 + calisLeo.calificacionEsp1 + calisLeo.calificacionIng1 + calisLeo.calificacionGeo1 + calisLeo.calificacionHist1 + calisLeo.calificacionCien1 + calisLeo.calificacionEco1;
+
+const numeroCalisLeo = Number(sumaCalisLeo)
+
+console.log(numeroCalisLeo)
+
+const promedioLeo = sumaCalisLeo / califasDeLeo.length;
+
+console.log("El promedio de " + califasDeLeo.nombre + " es de :" + promedioLeo)
+
 
