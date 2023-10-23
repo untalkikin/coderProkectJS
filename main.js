75afb7ddd328cba0c23c3c59677e42ad50709f71
Jose Enrique <kikedlt27@gmail.com>
2023-10-22 22:26:29 -0600

    Update for project Final
    Update.

--- main.js
+++ main.js
@@ -3,89 +3,25 @@ console.log("Simulador de calificaciones");
 //Saludo mediante alert
 alert("Hola bienvenido al simulador de alta de calificaciones")
 
-//Se declara el nombre del estudiante
-let nombreEstudiante = prompt("Ingresa el nombre del estudiante")
+//Variables
 
+const listarCalifas = document.querySelector('#listas-calis');
+const formulario = document.querySelector('#formulario');
+let calificaciones = [];
 
+//Funciones de eventos
+eventListeners();
 
-//Se declaran las variables
-let calificacionMate = parseInt(prompt("Ingresa la calificacion de matematicas"));
-let calificacionEsp = parseInt(prompt("Ingresa la calificacion de Español"));
-let calificacionIng = parseInt(prompt("Ingresa la calificacion de Ingles"));
-let calificacionGeo = parseInt(prompt("Ingresa la calificacion de Geografia"));
-
-
-var sumaCalifas = calificacionEsp + calificacionMate + calificacionIng + calificacionGeo;
-console.log(sumaCalifas);
-
-let promedio = sumaCalifas / 4;
-
-console.log(promedio)
-
-function acreditaSemestre() {
-  if (promedio <= 5) {
-    return "No acredita, el promedio es de: " + promedio;
-  } else {
-    return "Acredita el semestre, el promedio es de: " + promedio;
-  }
-}
-
-console.log(acreditaSemestre());
-
-while(promedio >= 9){
-  alert("Es un estudiantede excelentcia");
-  if(promedio < 9){
-    break;
-  }
-  break;
-}
-//Objetos
-const listaCalifas ={
-  calificacionHist : 9,
-  calificacionCien: 8,
-  calificacionEco:10
-}
-
-console.log(`${nombreEstudiante} obtuvo un ${calificacionMate} en Matematicas`);
-console.log(`${nombreEstudiante} obtuvo un ${calificacionEsp} en Español`);
-console.log(`${nombreEstudiante} obtuvo un ${calificacionIng} en Ingles`);
-console.log(`${nombreEstudiante} obtuvo un ${calificacionGeo} en Geografia`);
-console.log(`${nombreEstudiante} obtuvo un ${listaCalifas.calificacionHist} en Historia`);
-console.log(`${nombreEstudiante} obtuvo un ${listaCalifas.calificacionCien} en Ciencias`);
-console.log(`${nombreEstudiante} obtuvo un ${listaCalifas.calificacionEco} en Economia`);
-
-//Arreglos
-const califasDeLeo = [
-  {
-    nombre: "Leonel",
-    calificacionMate1: 8,
-    calificacionEsp1: 9,
-    calificacionIng1: 10,
-    calificacionGeo1: 8,
-    calificacionHist1: 9,
-    calificacionCien1: 10,
-    calificacionEco1: 10
-  }
-]
-
-let nuevaCalifa = { 
-  calificacionEti1 : 10 
-}
-
-califasDeLeo.push(nuevaCalifa)
-
-califasDeLeo.forEach((califa) => {
-  console.log(califa)
-})
-
-const nombreBuscado = prompt("Ingrese el nombre del estudiante que desea buscar:");
-
-let estudianteEncontrado = califasDeLeo.find(estudiante => estudiante.nombre === nombreBuscado);
-
-if (estudianteEncontrado) {
-  console.log("Estudiante encontrado:");
-  console.log(estudianteEncontrado);
-} else {
-  console.log("No se encontró ningún estudiante con ese nombre.");
+function eventListeners(){
+  //Cuando se envia el formuñlario
+  formulario.addEventListener('submit', agregarCali);
+  //Borrar calificacion
+  listarCalifas.addEventListener('click', borrarCali);
+  //Contenido cargado
+  document.addEventListener('DOMContentLoaded', () =>{
+    calis = JSON.parse( localStorage.getItem('calis') ) || []  ;
+    console.log(calis);
+    crearHTML();
+  });
 }
 
